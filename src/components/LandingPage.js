2514302d7c2c5893af278805f6f36b99387fb775
Josephine-Ann <josephineanngeoghegan@gmail.com>
2020-09-01 09:31:41 +0200

    clip-path

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,6 +3,11 @@ import FeatureList from './FeatureList';
 
 const LandingPage = () => (
   <div>
+    <div id="image-is">
+    </div>
+    <div id="header-text">
+      <h1>Jo and Cihad's awesome site</h1>
+    </div>
     <div id="triangle-bottomleft">
     </div>
     <div id="under-triangle">
