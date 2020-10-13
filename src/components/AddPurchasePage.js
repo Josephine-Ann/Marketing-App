c4e48fae43726b104afd34ce45a67ba9b0e19dc8
Chad Saglam <saglam.chd@gmail.com>
2020-10-13 15:53:32 +0200

    styling works well for now :)

--- src/components/AddPurchasePage.js
+++ src/components/AddPurchasePage.js
@@ -7,9 +7,13 @@ import selectPurchasesSummary from '../selectors/purchasesSummary';
 import CardDeck from 'react-bootstrap/CardDeck'
 import uuid from 'uuid';
 import { CartPageItemNoPic } from './CartPageItemNoPic';
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import CurrencyFormat from "react-currency-format";
+
 
 
 export class AddPurchasePage extends React.Component {
+
   onSubmit = (purchase) => {
     this.props.purchases.forEach((indPurchase) => {
       let id = indPurchase.id
@@ -23,33 +27,72 @@ export class AddPurchasePage extends React.Component {
   }
 
   render() {
+
     return (
-      <div id="add-purchase-area">
-        <PurchaseForm
-          className="add-purchase-form"
-          onSubmit={this.onSubmit}
-        />
-        {/* <CardDeck>
+      <div >
+        <div id="add-purchase-area">
+
+          <PurchaseForm
+            className="add-purchase-form"
+            onSubmit={this.onSubmit}
+          />
+          {/* <CardDeck>
           {this.props.features.map((feature) => {
             return <FeatureListItemCheckout key={uuid()} {...feature} />;
           })}
         </CardDeck> */}
-        <CardDeck >
-          return <div key={uuid()} className="add-purchase-cart-item">
-            {
-              this.props.purchases.length === 0 ? (
-                <div>
-                  <p>No purchases</p>
-                </div>
-              ) : (
-                  this.props.purchases.map((purchases) => {
-                    return <CartPageItemNoPic key={uuid()} {...purchases} onClick={this.onClick} />;
-                  })
-                )
-            }
-          </div>;
-        </CardDeck>
+          <CardDeck >
+            return <div key={uuid()} className="add-purchase-cart-item">
+              {
+                this.props.purchases.length === 0 ? (
+                  <div>
+                    <p>No purchases</p>
+                  </div>
+                ) : (
+                    this.props.purchases.map((purchases) => {
+                      return <CartPageItemNoPic key={uuid()} {...purchases} onClick={this.onClick} />;
+                    })
+                  )
+              }
+            </div>;
+          </CardDeck>
+        </div>
+
+        {/* Payment section - Payment method */}
+        <div className='payment__section'>
+          <div className="payment__title">
+            <h4>Payment Method</h4>
+          </div>
+          <div className="payment__details">
+            {/* Stripe magic will go */}
+
+            <form >
+              <CardElement />
+
+              {/* <div className='payment__priceContainer'>
+                <CurrencyFormat
+                  renderText={(value) => (
+                    <h3>Order Total: {value}</h3>
+                  )}
+                  decimalScale={2}
+                  value={2333}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"$"}
+                />
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                </button>
+              </div> */}
+
+              {/* Errors */}
+              {/* {error && <div>{error}</div>} */}
+            </form>
+          </div>
+        </div>
+
       </div>
+
     );
   }
 }
